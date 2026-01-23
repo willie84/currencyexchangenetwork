37585d842fd17adadddc20d25153d9ac636b25b2
Willie Macharia <williemacharia@visitor-10-59-159-200.wifi.berkeley.edu>
2026-01-22 18:26:15 -0800

    Added offer webhook

--- pages/api/requests/[requestId]/offers.js
+++ pages/api/requests/[requestId]/offers.js
@@ -94,6 +94,7 @@ export default async function handler(req, res) {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           offer: item,
+          offerAmount: item.offerAmount,
           responder: {
             name: item.responderName,
             email: item.responderEmail,
